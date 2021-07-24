b35be6a0728728884596e9086a319568f97af281
aybek can kaya <aybek.kaya@rapsodo.com>
2021-07-24 03:37:09 +0300

    pek

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ChildViewController",
-            dependencies: []),
+            dependencies: ["DeclarativeLayout"]),
         .testTarget(
             name: "ChildViewControllerTests",
             dependencies: ["ChildViewController"]),
